28ce1b8afdc7207682c1a58d2a3ec307566a904e
Emanuele Gorga <emanuelegorga@gmail.com>
2022-09-08 13:59:43 +0100

    Implement ConfigModule and ConfigSERVICE

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -18,12 +18,18 @@ import Task from './task.entity';
 import { GetUser } from '../auth/get-user.decorator';
 import { User } from '../auth/user.entity';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
   private logger = new Logger('TasksController');
-  constructor(private tasksService: TasksService) {}
+  constructor(
+    private tasksService: TasksService,
+    private configService: ConfigService,
+  ) {
+    console.log(configService.get('ENVIRONMENT'));
+  }
 
   @Get()
   getTasks(
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -5,9 +5,10 @@ import { TasksRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
 import Task from './task.entity';
 import { AuthModule } from '../auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
+  imports: [ConfigModule, TypeOrmModule.forFeature([Task]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService, TasksRepository],
 })
