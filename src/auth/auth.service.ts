84adff7d633df072cbcd0a5a9a50ed14b83ce041
Emanuele Gorga <emanuelegorga@gmail.com>
2022-09-08 07:52:03 +0100

    Create a User

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @InjectRepository(UsersRepository)
-    private usersRpoe: UsersRepository,
-  ) {}
+  constructor(private usersRepository: UsersRepository) {}
+
+  async signup(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @Injectable()
@@ -9,4 +10,12 @@ export class UsersRepository {
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    await this.usersRepository.save(user);
+  }
 }
