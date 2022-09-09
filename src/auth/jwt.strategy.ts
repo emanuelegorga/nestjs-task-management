e8f11bd8ff0f0b50fbb383628f5c3765ffc82bc2
Emanuele Gorga <emanuelegorga@gmail.com>
2022-09-09 07:21:38 +0100

    Provide JWT Token using Config Module and environment variables

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,12 +4,16 @@ import { Strategy, ExtractJwt } from 'passport-jwt';
 import { JwtPayload } from './jwt-payload.interface';
 import { User } from './user.entity';
 import { UsersRepository } from './users.repository';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private usersRepository: UsersRepository) {
+  constructor(
+    private usersRepository: UsersRepository,
+    private configService: ConfigService,
+  ) {
     super({
-      secretOrKey: 'topSecret51',
+      secretOrKey: configService.get('JWT_SECRET'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
